9b53da247b9cc040a5841279ca1df379c465a3a5
Florian Krull <florian.krull@student.uibk.ac.at>
2018-04-11 16:58:49 +0200

    Fixed order by

--- src/main/java/eu/transkribus/report/ReportFromDatabase.java
+++ src/main/java/eu/transkribus/report/ReportFromDatabase.java
@@ -367,7 +367,7 @@ public class ReportFromDatabase implements ReportDatabaseInterface {
 		FimgStoreUriBuilder uriBuilder = FimgStoreReadConnection.getUriBuilder();
 
 		String sql = "select * from actions a join session_history sh on a.session_history_id = sh.session_history_id where a.type_id = 2 AND a.client_Id is null  AND time between ? and ? order by time desc";
-		String sql2 = "select * from images,doc_md where created between ? and ? order by created desc  ";
+		String sql2 = "select * from images,DOC_MD where created between ? and ? ";
 		String sql3 = "select * from jobs where started between ? and ? order by started desc";
 
 		HSSFWorkbook workbook = new HSSFWorkbook();
@@ -411,6 +411,8 @@ public class ReportFromDatabase implements ReportDatabaseInterface {
 			String uri = uriBuilder.getImgUri(imageKey, ImgType.browser).toString();
 			String imageFile = rs2.getString("imgfilename");
 			String uploader = rs2.getString("uploader");
+			String title = rs2.getString("title");
+			String author = rs2.getString("author");
 			int width = rs2.getInt("width");
 			int height = rs2.getInt("height");
 			String created2 = rs2.getString("created");
@@ -427,7 +429,7 @@ public class ReportFromDatabase implements ReportDatabaseInterface {
 			excelData.put(Integer.toString(rowCount),
 					new Object[] { actionId, userLogin, userAgent, ip, created, destroyed, guiVersion });
 			excelData2.put(Integer.toString(rowCount),
-					new Object[] { imageId, uri, imageFile, uploader, width, height, created2 });
+					new Object[] { imageId, uri, imageFile, uploader, title, author, width, height, created2 });
 			excelData3.put(Integer.toString(rowCount),
 					new Object[] { jobid, userid, type, description, pages, module, started, ended });
 
